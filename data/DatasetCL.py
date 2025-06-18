dcd6f5b11b9999f8d271e7a036801f22927d459a
Ray-Shu <rayshu1015@gmail.com>
2025-06-18 14:48:02 -0600

    fixed DatasetCL to properly give batches when using torch Dataloaders.

--- data/DatasetCL.py
+++ data/DatasetCL.py
@@ -1,6 +1,5 @@
 import torch 
 from utils.tensor_utils import convert_batch_to_tensor
-torch.utils.data.Dataset
 class DatasetCL(torch.utils.data.Dataset): 
     def __init__(self, sampler, batch_size: int, k: int = 2): 
         """
@@ -14,15 +13,15 @@ class DatasetCL(torch.utils.data.Dataset):
         self.batch_size = batch_size 
         self.k = k 
 
-        self.batch = convert_batch_to_tensor(self.sampler.sample_batch(batch_size=self.batch_size, k=self.k))
-    
+        anchor, positive = convert_batch_to_tensor(self.sampler.sample_batch(batch_size=self.batch_size, k=self.k))
+        self.pairs = list(zip(anchor, positive))
+
     def __len__(self): 
-        return len(self.batch[0])
+        return len(self.pairs)
     
     def __getitem__(self, index):
-        s_i = self.batch[0][index] 
-        s_j = self.batch[1][index] 
-        return torch.as_tensor(s_i, dtype=torch.float32), torch.as_tensor(s_j, dtype=torch.float32)
+        s_i, s_j = self.pairs[index] 
+        return (torch.as_tensor(s_i, dtype=torch.float32), torch.as_tensor(s_j, dtype=torch.float32))
     
     def get_batch(self):
-        return self.batch
+        return self.pairs
