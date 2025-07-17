fedc78e41feed965ea810273810934dff5543d83
Ray Shu <rayshu1015@gmail.com>
2025-07-17 10:14:11 -0600

    updated dataset class to sample all states from the minari dataset

--- data/StatesDataset.py
+++ data/StatesDataset.py
@@ -1,20 +1,47 @@
 from data.Sampler import Sampler 
 import torch
+import numpy as np
+import minari
 
-class StatesDataset(torch.utils.data.Dataset): 
-    def __init__(self, cl_model,  sampler: Sampler, num_states: int):
+class RandomSamplingDataset(torch.utils.data.Dataset): 
+    def __init__(self, cl_model, sampler: Sampler = None,  minari_dataset_id = "D4RL/pointmaze/large-v2", num_states: int = None, iterate_thru_dataset: bool = False):
         """
         Creates a dataset with randomly sampled states from the minari dataset specified by the TrajectorySet class. 
         """
         self.cl_model = cl_model 
+        self.minari_dataset = minari.load_dataset(minari_dataset_id)
+        self.sampler = sampler 
 
-        states = torch.tensor(sampler.sample_states(batch_size=num_states), dtype=torch.float32)
+        if iterate_thru_dataset: 
+            self.states = self.__get_all_states()
+            self.states = torch.as_tensor(self.states, dtype=torch.float32)
 
-        with torch.no_grad():
-            self.z = cl_model(states)
+        else: 
+            assert num_states != None, "If iterate_thru_dataset is False, must set num_states to a positive nonzero integer."
+            assert sampler != None, "If iterate_thru_dataset is False, must have a sampler to sample states."
+            self.states = torch.tensor(sampler.sample_states(batch_size=num_states), dtype=torch.float32)
+
+        self.z = cl_model(self.states)
 
     def __len__(self): 
         return len(self.z) 
     
     def __getitem__(self, index):
         return self.z[index]
+
+    def __get_all_states(self): 
+        """
+        Gets all of the states from the minari dataset and returns all states in Tensor form. 
+        """
+        total_eps = self.minari_dataset.total_episodes
+        print(total_eps)
+
+        eps = self.minari_dataset.sample_episodes(n_episodes=total_eps) 
+        states = eps[0].observations["observation"]
+
+        # stack all states vertically so the states array has shape: [N, 4], where N is the total number of states
+        for i in range(1, total_eps): 
+            states = np.vstack((states, eps[i].observations["observation"]))
+
+        return states
+
